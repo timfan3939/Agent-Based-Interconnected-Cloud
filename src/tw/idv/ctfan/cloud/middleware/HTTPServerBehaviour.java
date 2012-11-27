ca56f8ce18b7b2e19d97e2602dbadb1299646fd1
Chih-Tien Fan <timfan3939@gmail.com>
2012-11-27 20:38:22 +0800

    HTTP Post Parser ready, form data and submitter is not ready yet

--- src/tw/idv/ctfan/cloud/middleware/HTTPServerBehaviour.java
+++ src/tw/idv/ctfan/cloud/middleware/HTTPServerBehaviour.java
@@ -1,9 +1,14 @@
 package tw.idv.ctfan.cloud.middleware;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
 
 import tw.idv.ctfan.cloud.middleware.policy.Policy;
@@ -41,13 +46,16 @@ public class HTTPServerBehaviour extends CyclicBehaviour {
 	@Override
 	public void action() {
 		try {
-			Socket s = server.accept();
-			BufferedInputStream input  = new BufferedInputStream(s.getInputStream());
-			boolean endOfHeader = false;
+			Socket client = server.accept();
+			
+			boolean endOfHeader = false;		
+			
+			BufferedInputStream input = new BufferedInputStream(client.getInputStream());
 			
 			int connectionType = 0;
 			long contentLength = 0;
 			String file = "";
+			String boundary = "";
 			int line = 0;
 			
 			do {
@@ -78,44 +86,266 @@ public class HTTPServerBehaviour extends CyclicBehaviour {
 				if(line == 1) {
 					if(info[0].matches("GET")) {
 						connectionType = HTTP_GET;
-						file = info[0];					
+						file = info[1];					
 					} else if(info[0].matches("POST")) {
 						connectionType = HTTP_POST;
-						file = info[0];
+						file = info[1];
 					} else {
-						PrintStream output = new PrintStream(s.getOutputStream());
-						output.print("HTTP/1.0 501 Not Implemented" + EOL);
-						output.print("Server: tw.idv.ctfan.cloud.middleware.HTTPServerBehaviour" + EOL);
-						output.print("Date: " + new Date() + EOL);
-						output.print("Content-Type: text/html" + EOL);
-						output.print(EOL);
-						output.print("<HTML><HEAD><TITLE>Error 501 Not Implemented</TITLE></HEAD>");
-						output.print("<BODY><H2>501 Not Implemented</H2>");
-						output.print("<p />" + field + "</BODY></HTML>");
-						output.flush();
-						s.close();
+						myAgent.addBehaviour(new Error501Response(myAgent, client));
 						return;
 					}
 				} else {
 					if(info[0].matches("Content-Length:")) {
 						contentLength = Long.parseLong(info[1]);
 					}
+					else if(info[0].matches("Content-Type:")) {
+						if(info[1].matches("multipart/form-data;")) {
+							String bound[] = info[2].split("=");
+							if(bound.length == 2 && bound[0].matches("boundary"))
+								boundary = bound[1];
+							else {
+								myAgent.addBehaviour(new Error501Response(myAgent, client));
+								return;
+							}
+						}
+						else {
+							myAgent.addBehaviour(new Error501Response(myAgent, client));
+							return;
+						}							
+					}
 				}				
-			} while(!endOfHeader);	
+			} while(!endOfHeader);
+			
+			System.out.println("Reading header done");
+			
+			// Read body content
+			ByteArrayOutputStream bodyContentStream;
+			int size = 0;
+			
+			if(connectionType==HTTP_POST) {
+				boolean endOfMessage = false;
+				int state = 0x301;
+				final int STATE_BOUNDARY = 0x301;
+				final int STATE_DESCRIPTOR = 0x302;
+				final int STATE_READ_DATA = 0x303;
+				
+				do {
+					switch (state) {
+					case STATE_BOUNDARY: {
+						bodyContentStream = new ByteArrayOutputStream();
+						boolean endOfLine = false;
+						while (!endOfLine) {
+							int ch = input.read();
+							if(ch == '\r')
+								if( (ch=input.read()) =='\n' ){
+									endOfLine = true;
+								} else {
+									bodyContentStream.write('r');
+									bodyContentStream.write(ch);
+								}
+							else
+								bodyContentStream.write(ch);
+						}
+						String b = new String(bodyContentStream.toByteArray());
+						if(b.compareTo("--"+boundary)==0) {
+							System.out.println("--"+boundary);
+							state = STATE_DESCRIPTOR;
+						} else if(b.compareTo("--"+boundary+"--")==0){
+							System.out.println("--"+boundary+"--");
+							endOfMessage = true;
+						} else if(b.compareTo(boundary)==0){
+							System.out.println(boundary);
+							state = STATE_DESCRIPTOR;
+						} else if(b.compareTo(boundary+"--")==0){
+							System.out.println(boundary+"--");
+							endOfMessage = true;
+						}
+						bodyContentStream = null;
+					}	break;
+					case STATE_DESCRIPTOR:{
+						bodyContentStream = new ByteArrayOutputStream();
+						boolean endOfLine = false;
+						while (!endOfLine) {
+							int ch = input.read();
+							if(ch == '\r')
+								if( (ch=input.read()) =='\n' ){
+									endOfLine = true;
+								} else {
+									bodyContentStream.write('r');
+									bodyContentStream.write(ch);
+								}
+							else
+								bodyContentStream.write(ch);
+						}
+						
+						if(bodyContentStream.size()==0)
+							state = STATE_READ_DATA;
+						else
+							System.out.println(new String(bodyContentStream.toByteArray()));
+						bodyContentStream = null;
+					}	break;
+					case STATE_READ_DATA:{
+						bodyContentStream = new ByteArrayOutputStream();
+						boolean[] passes = new boolean[4];
+						for (boolean b:passes) {
+							b = false;
+						}
+						while(!passes[3]){
+							int ch = input.read();
+							if(!passes[0]) {
+								if(ch=='\r')
+									passes[0] = true;
+								else
+									bodyContentStream.write(ch);
+							} else {
+								if(!passes[1]) {
+									if(ch=='\n')
+										passes[1] = true;
+									else {
+										bodyContentStream.write('\r');
+										bodyContentStream.write(ch);
+										passes[0] = false;
+									}
+								} else {
+									if(!passes[2]) {
+										if(ch=='-')
+											passes[2] = true;
+										else {
+											bodyContentStream.write('\r');
+											bodyContentStream.write('\n');
+											bodyContentStream.write(ch);
+											passes[0] = passes[1] = false;
+										}
+									} else {
+										if(ch == '-')
+											passes[3] = true;
+										else {
+											bodyContentStream.write('\r');
+											bodyContentStream.write('\n');
+											bodyContentStream.write('-');
+											bodyContentStream.write(ch);
+											passes[0] = passes[1] = passes[2] = false;
+										}
+									}
+								}
+							}
+						}
+						System.out.println("Got Binary size " + bodyContentStream.size());
+						bodyContentStream = null;
+						state = STATE_BOUNDARY;
+					}	break;
+					}					
+					
+				} while (!endOfMessage);
+			}
+			
+			System.out.println("Got " + size + " bytes of message");
+			System.out.println("Reading message done");
+			
 			
 			if(connectionType==HTTP_GET ) {
-				if(file.equals("/"))
-					myAgent.addBehaviour(new StatusResponse(myAgent, s));
-//				else
-//					myAgent.addBehaviour(new GetFileResponse(myAgent, s));
+				if(file.equals("/")) {
+					System.out.println("StatusResponse");
+					myAgent.addBehaviour(new StatusResponse(myAgent, client));
+				}
+//				else {
+//					myAgent.addBehaviour(new GetFileResponse(myAgent, client));
+//				}
 			} else if(connectionType==HTTP_POST) {
-//				myAgent.addBehaviour(new UploadFileResponse(myAgent, s, input));
+				if(file.equals("/submit") && !boundary.isEmpty()){
+					System.out.println("UploadFileResponse");
+					myAgent.addBehaviour(new UploadFileResponse(myAgent, client, new byte[0], boundary));
+				}
+				else {
+					System.out.println("StatusResponse");
+					myAgent.addBehaviour(new StatusResponse(myAgent, client));
+				}
 			}
+			
+			System.out.println("ClientMessageReceieveBehaviour Terminated");
 			
 			
 		} catch(Exception e){
 			e.printStackTrace();
 		}		
+	}
+	
+	private class Error501Response extends OneShotBehaviour {
+		private static final long serialVersionUID =1L;
+		Socket client;
+		public Error501Response(Agent a, Socket s){
+			super(a);
+			client = s;		
+		}
+		@Override
+		public void action() {
+			try {
+				PrintStream output = new PrintStream(client.getOutputStream());
+				output.print("HTTP/1.0 501 Not Implemented" + EOL);
+				output.print("Server: tw.idv.ctfan.cloud.middleware.HTTPServerBehaviour" + EOL);
+				output.print("Date: " + new Date() + EOL);
+				output.print("Content-Type: text/html" + EOL);
+				output.print(EOL);
+				output.print("<HTML><HEAD><TITLE>Error 501 Not Implemented</TITLE></HEAD>");
+				output.print("<BODY><H2>501 Not Implemented</H2>");
+				output.print("<p />" + "Error" + "</BODY></HTML>");
+				output.flush();
+			} catch (Exception e) {
+				System.err.println("HTTPServerBehaviour::Error501Response::action::out Error");
+				e.printStackTrace();
+			} finally {
+				try {
+					client.close();
+				} catch (Exception e) {
+					System.err.println("HTTPServerBehaviour::Error501Response::client closing error");
+					e.printStackTrace();
+				}
+			}
+		}		
+	}
+	
+	private class UploadFileResponse extends OneShotBehaviour {
+		private static final long serialVersionUID =1L;
+		Socket client;
+		byte[] inputByte;
+		byte[] boundaryByte;
+		String boundary;
+		
+		public UploadFileResponse(Agent a, Socket s, byte[] in, String bound){
+			super(a);
+			client = s;
+			inputByte = in.clone();
+			boundary = bound;
+			boundaryByte = boundary.getBytes();
+		}
+
+		@Override
+		public void action() {
+			System.out.println("Start UploadFileResponse");
+			try {
+				System.out.println("Boundary: " + boundary);
+				
+//				int start, end = 0;
+//				int line=0;
+//				boolean bound = true;
+//				
+//				for(int i=0; i<inputByte.length; i++) {
+//					if(bound = true) {
+//						for(int j=0; j<inputB; j++) {
+//							
+//						}
+//					}
+//				}
+				
+								
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				myAgent.addBehaviour(new StatusResponse(myAgent, client));
+			}
+			
+		}
 	}
 	
 	private class StatusResponse extends OneShotBehaviour {
@@ -130,6 +360,7 @@ public class HTTPServerBehaviour extends CyclicBehaviour {
 		@Override
 		public void action() {
 			try {
+				System.out.println("StatusResponse Start");
 				PrintStream output = new PrintStream(client.getOutputStream());
 				
 				//=====
@@ -152,13 +383,28 @@ public class HTTPServerBehaviour extends CyclicBehaviour {
 				
 				// Start of Body
 				output.print("<BODY>");
-				
-				// cluster+running list
+								
+				// LOGOS
 				output.print("<DIV>");
-				output.print("<H1><img src=\"http://dmclab.csie.ntpu.edu.tw/web/media/logo_action.gif\" />Hybrid Cloud Information Viewer</H1>");
+				//output.print("<H1><img src=\"http://dmclab.csie.ntpu.edu.tw/web/media/logo_action.gif\" />Hybrid Cloud Information Viewer</H1>");
 				output.print("<h3>Copyright: C.T.Fan</h3>");
 				output.print("<h3>Uptime: "+ m_initTime.toString() +"</h3>");
 				output.print("</DIV>");
+				output.print("<HR/>");
+
+				// Submit form
+				output.print("<DIV>");
+				output.print("<H3>Submit Job</H3><BR/>");
+				output.print("<FORM action=\"submit\" method=\"post\" enctype=\"multipart/form-data\">");
+				output.print("Binary File: <INPUT type=\"file\" name=\"binaryFile\" accept=\"application/java-archive\" />");
+				output.print("Parameter: <INPUT type=\"text\" name=\"parameter\" />");
+				output.print("<INPUT type=\"submit\" value=\"submit\" />");
+				output.print("</FORM>");
+				output.print("</DIV>");
+				output.print("<HR/>");
+				
+
+				// cluster+running list
 				output.print("<DIV>");
 				output.print("<H1>Private Cluster/Job Information</H1>");
 				output.print("<TABLE style=\"text-align:center; border-collapse:collapse; border:1px black solid; width:100%\">");
