9d27b3f0267de17bb7587f8d3d92465b9e7edf17
C.T.Fan@NTPU114 <timfan3939@gmail.com>
2013-09-08 15:51:46 +0800

    JobAgent Base is added

---
+++ src/tw/idv/ctfan/cloud/middleware/Cluster/JobAgent.java
@@ -0,0 +1,121 @@
+package tw.idv.ctfan.cloud.middleware.Cluster;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public abstract class JobAgent extends Agent {
+
+	private static final long serialVersionUID = 1L;
+	protected String masterName;
+	boolean startedYet = false;
+	boolean finishedYet= false;
+	
+	private FileOutputStream Log;
+	
+	protected void WriteLog(String s){
+		try {
+			Log.write(s.getBytes());
+			Log.write('\n');
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected void setup() {
+		try {
+			Log = new FileOutputStream("/home/hadoop/ctfan/log/" + getLocalName() + ".log", false);
+		} catch (FileNotFoundException e) {
+			System.err.println("Error while opeing login file");
+			e.printStackTrace();
+		}
+		
+		addBehaviour(new HeartBeatBehaviour(this, 3000));
+		addBehaviour(new ListeningBehaviour(this));
+	}
+	
+	protected String getMasterName() {
+		return masterName;
+	}
+
+	protected void setMasterName(String masterName) {
+		this.masterName = masterName;
+	}
+
+	private class HeartBeatBehaviour extends TickerBehaviour {
+	private static final long serialVersionUID = 1L;
+
+		public HeartBeatBehaviour(Agent a, int t) {
+			super(a, t);
+		}
+
+		@Override
+		protected void onTick() {
+			ACLMessage msg = new ACLMessage(ACLMessage.CONFIRM);
+			msg.addReceiver(new AID(masterName + "@" + myAgent.getHap(), AID.ISGUID));
+			if(!finishedYet)
+				msg.setContent(OnHeartBeat());
+			else {
+				msg.setContent("FINISHED");
+			}
+			
+			myAgent.send(msg);			
+		}
+	}
+	
+	private class ListeningBehaviour extends CyclicBehaviour {
+		private static final long serialVersionUID = 1L;
+
+		public ListeningBehaviour(Agent a) {
+			super(a);
+		}
+		
+		@Override
+		public void action() {
+			try {
+				ACLMessage msg = myAgent.receive(MessageTemplate.MatchAll());
+				if(msg == null) {
+					block();
+					return;
+				}
+				
+				switch(msg.getPerformative()) {
+				case ACLMessage.INFORM:{
+					WriteLog("Got Migrate Message");
+					// TODO: add migrate procedure here
+				} break;
+				case ACLMessage.CONFIRM: {
+					WriteLog("Got Start Job Message");
+					if(startedYet) {
+						System.err.println("Job has been started");
+					} else if(finishedYet){
+						myAgent.doDelete();
+					} else{
+						StartJob();
+						startedYet = true;
+						finishedYet = false;
+					}					
+				} break;
+				}
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}			
+		}		
+	}
+	
+	protected void JobFinished() {
+		finishedYet = true;
+	}
+	
+	protected abstract void StartJob();
+	
+	protected abstract String OnHeartBeat();
+
+}
