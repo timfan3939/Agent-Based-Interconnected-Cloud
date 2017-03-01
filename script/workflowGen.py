d2615b54a49f5babbccd706214527ada8ba29b06
C.T.Fan <timfan3939@gmail.com>
2017-03-01 11:43:43 +0800

    update workflow generator.  four workflows are added

--- script/workflowGen.py
+++ script/workflowGen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+	#!/usr/bin/env python3
 
 import json
 import math
@@ -17,12 +17,368 @@ import random
 def main():
 	#item = EpigenmoicsWorkflow()
 	item = MontageWorkflow()
+	#item = LIGOWorkflow()
+	#item = CyberShakeWorkflow()
+	
+	item.generate(4)
+	item.generate(6)
+	item.generate(8)
 	item.generate(10)
+	item.generate(12)
 	
 def tasknameGen(note, id):
 	return 'task_{}_{:0>5}'.format(note, id)
+	
+class CyberShakeWorkflow:
+	task_size = 5
+	def gen_ExtractSGT(self, id, parallelism):
+		tasks = []
+		for i in range(2):
+			task = {}
+			task['taskname'] = tasknameGen('ExtractSGT', id)
+			task['program'] = 'DummyTask.jar'
+			task['task_id'] = id
+			task['command'] = self.task_size
+			task['_outE'] = []
+			task['_inE'] = []
+			
+			tasks.append(task)
+			id += 1
+		
+		self.ExtractSGT = tasks
+		return id
+	
+	def gen_SeismogramSynthesis(self, id, parallelism):
+		tasks = []
+		for i in range(parallelism):
+			task = {}
+			task['taskname'] = tasknameGen('ExtractSGT', id)
+			task['program'] = 'DummyTask.jar'
+			task['task_id'] = id
+			task['command'] = self.task_size
+			task['_outE'] = []
+			task['_inE'] = []
+			
+			tasks.append(task)
+			id += 1
+		
+		self.SeismogramSynthesis = tasks
+		return id
+	
+	def gen_ZipSeis(self, id, parallelism):
+		tasks = []
+		task = {}
+		task['taskname'] = tasknameGen('ZipSeis', id)
+		task['program'] = 'DummyTask.jar'
+		task['task_id'] = id
+		task['command'] = self.task_size
+		task['_outE'] = []
+		task['_inE'] = []
+			
+		tasks.append(task)
+		id += 1
+		
+		self.ZipSeis = tasks
+		return id
+	
+	def gen_PeakValCalcOkaya(self, id, parallelism):
+		tasks = []
+		for i in range(parallelism):
+			task = {}
+			task['taskname'] = tasknameGen('PeakValCalcOkaya', id)
+			task['program'] = 'DummyTask.jar'
+			task['task_id'] = id
+			task['command'] = self.task_size
+			task['_outE'] = []
+			task['_inE'] = []
+			
+			tasks.append(task)
+			id += 1
+		
+		self.PeakValCalcOkaya = tasks
+		return id
+	
+	def gen_ZipPSA(self, id, parallelism):
+		tasks = []
+		task = {}
+		task['taskname'] = tasknameGen('ZipPSA', id)
+		task['program'] = 'DummyTask.jar'
+		task['task_id'] = id
+		task['command'] = self.task_size
+		task['_outE'] = []
+		task['_inE'] = []
+			
+		tasks.append(task)
+		id += 1
+		
+		self.ZipPSA = tasks
+		return id
+	
+	def connect_ExtractSGT_SeismogramSynthesis(self):
+		a_tasks= self.ExtractSGT
+		b_tasks = self.SeismogramSynthesis
+		
+		a_len = len(a_tasks)
+		b_len = len(b_tasks)
+		
+		count = 0
+		
+		for i in range(b_len):
+			a = a_tasks[i%a_len]
+			b = b_tasks[i]
+			a['_outE'].append( {'_inV':b['task_id']} )
+			b['_inE'].append( {'_outV':a['task_id']} )	
+		
+		pass
+	
+	def connent_SeismogramSynthesis_ZipSeis(self):
+		a_tasks = self.SeismogramSynthesis
+		b_tasks = self.ZipSeis
+		
+		b = b_tasks[0]		
+		for a in a_tasks:
+			a['_outE'].append( {'_inV':b['task_id']} )
+			b['_inE'].append( {'_outV':a['task_id']} )			
+		pass
+	
+	def connect_SeismogramSynthesis_PeakValCalcOkaya(self):
+		a_tasks = self.SeismogramSynthesis
+		b_tasks = self.PeakValCalcOkaya
+		
+		for i in range( len(a_tasks) ):
+			a = a_tasks[i]
+			b = b_tasks[i]
+			a['_outE'].append( {'_inV':b['task_id']} )
+			b['_inE'].append( {'_outV':a['task_id']} )			
+		pass
+	
+	def connect_PeakValCalcOkaya_ZipPSA(self):
+		a_tasks = self.PeakValCalcOkaya
+		b_tasks = self.ZipPSA
+		
+		b = b_tasks[0]		
+		for a in a_tasks:
+			a['_outE'].append( {'_inV':b['task_id']} )
+			b['_inE'].append( {'_outV':a['task_id']} )			
+		pass
+	
+	def generate(self, parallelism):
+		id = 1
+		
+		id = self.gen_ExtractSGT(id, parallelism)
+		id = self.gen_SeismogramSynthesis(id, parallelism)
+		id = self.gen_ZipSeis(id, parallelism)
+		id = self.gen_PeakValCalcOkaya(id, parallelism)
+		id = self.gen_ZipPSA(id, parallelism)
+				
+		print('Total task generated:', id-1)
+		
+		self.connect_ExtractSGT_SeismogramSynthesis()
+		self.connent_SeismogramSynthesis_ZipSeis()
+		self.connect_SeismogramSynthesis_PeakValCalcOkaya()
+		self.connect_PeakValCalcOkaya_ZipPSA()		
+		
+		self.workflow = {'taskworkflow':[]}
+		self.workflow['taskworkflow'] += self.ExtractSGT
+		self.workflow['taskworkflow'] += self.SeismogramSynthesis
+		self.workflow['taskworkflow'] += self.ZipSeis
+		self.workflow['taskworkflow'] += self.PeakValCalcOkaya
+		self.workflow['taskworkflow'] += self.ZipPSA
+		
+		json.dumps(self.workflow, indent=4)
+		filename = 'workflow CyberShake {} tasks size {} parallelism {}.json'.format(id-1, self.task_size, parallelism)
+		json.dump(self.workflow, open(filename, encoding='utf8', mode='w'), indent=4)		
+		
+		pass
+	
+	pass
+	
+class LIGOWorkflow:
+	task_size = 5
+	def gen_TmpltBank(self, id, parallelism):
+		tasks = []
+		for i in range(parallelism):
+			task = {}
+			task['taskname'] = tasknameGen('TmpltBank', id)
+			task['program'] = 'DummyTask.jar'
+			task['task_id'] = id
+			task['command'] = self.task_size
+			task['_outE'] = []
+			task['_inE'] = []
+			
+			tasks.append(task)
+			id += 1
+		
+		self.TmpltBank = tasks
+		
+		return id
+	
+	def gen_Inspiral_phase1(self, id, parallelism):
+		tasks = []
+		for i in range(parallelism):
+			task = {}
+			task['taskname'] = tasknameGen('Inspiral_phase1', id)
+			task['program'] = 'DummyTask.jar'
+			task['task_id'] = id
+			task['command'] = self.task_size
+			task['_outE'] = []
+			task['_inE'] = []
+			
+			tasks.append(task)
+			id += 1
+		
+		self.Inspiral_phase1 = tasks
+		
+		return id
+	
+	def gen_Thinca_phase1(self, id, parallelism):
+		tasks = []
+		size = math.ceil(parallelism/5)
+		for i in range(size):
+			task = {}
+			task['taskname'] = tasknameGen('Thinca_phase1', id)
+			task['program'] = 'DummyTask.jar'
+			task['task_id'] = id
+			task['command'] = self.task_size
+			task['_outE'] = []
+			task['_inE'] = []
+			
+			tasks.append(task)
+			id += 1
+		
+		self.Thinca_phase1 = tasks
+		
+		return id
+	
+	def gen_TrigBank(self, id, parallelism):
+		tasks = []
+		for i in range(parallelism):
+			task = {}
+			task['taskname'] = tasknameGen('TrigBank', id)
+			task['program'] = 'DummyTask.jar'
+			task['task_id'] = id
+			task['command'] = self.task_size
+			task['_outE'] = []
+			task['_inE'] = []
+			
+			tasks.append(task)
+			id += 1
+		
+		self.TrigBank = tasks
+		
+		return id
+	
+	def gen_Inspiral_phase2(self, id, parallelism):
+		tasks = []
+		for i in range(parallelism):
+			task = {}
+			task['taskname'] = tasknameGen('Inspiral_phase2', id)
+			task['program'] = 'DummyTask.jar'
+			task['task_id'] = id
+			task['command'] = self.task_size
+			task['_outE'] = []
+			task['_inE'] = []
+			
+			tasks.append(task)
+			id += 1
+		
+		self.Inspiral_phase2 = tasks
+		
+		return id
+	
+	def gen_Thinca_phase2(self, id, parallelism):
+		tasks = []
+		size = math.ceil(parallelism/5)
+		for i in range(size):
+			task = {}
+			task['taskname'] = tasknameGen('Thinca_phase2', id)
+			task['program'] = 'DummyTask.jar'
+			task['task_id'] = id
+			task['command'] = self.task_size
+			task['_outE'] = []
+			task['_inE'] = []
+			
+			tasks.append(task)
+			id += 1
+		
+		self.Thinca_phase2 = tasks
+		
+		return id
+		
+	def connect_TmpltBank_Inspiral_phase1(self):		
+		for i in range(len(self.TmpltBank)):
+			a = self.TmpltBank[i]
+			b = self.Inspiral_phase1[i]
+			a['_outE'].append( {'_inV':b['task_id']} )
+			b['_inE'].append( {'_outV':a['task_id']} )
+		pass
+	
+	def connect_Inspiral_phase1_Thinca_phase1(self):
+		for i in range( len(self.Inspiral_phase1) ):
+			a = self.Inspiral_phase1[i]
+			b = self.Thinca_phase1[math.floor(i/5)]
+			a['_outE'].append( {'_inV':b['task_id']} )
+			b['_inE'].append( {'_outV':a['task_id']} )
+		pass
+		
+	def connect_Thinca_phase1_TrigBank(self):
+		for i in range( len(self.TrigBank) ):
+			b = self.TrigBank[i]
+			a = self.Thinca_phase1[math.floor(i/5)]
+			a['_outE'].append( {'_inV':b['task_id']} )
+			b['_inE'].append( {'_outV':a['task_id']} )	
+		pass
+		
+	def connect_TrigBank_Inspiral_phase2(self):		
+		for i in range(len(self.TrigBank)):
+			a = self.TrigBank[i]
+			b = self.Inspiral_phase2[i]
+			a['_outE'].append( {'_inV':b['task_id']} )
+			b['_inE'].append( {'_outV':a['task_id']} )
+		pass
+		
+	def connect_Inspiral_phase2_Thinca_phase2(self):
+		for i in range( len(self.Inspiral_phase2) ):
+			a = self.Inspiral_phase2[i]
+			b = self.Thinca_phase2[math.floor(i/5)]
+			a['_outE'].append( {'_inV':b['task_id']} )
+			b['_inE'].append( {'_outV':a['task_id']} )
+		pass
+		
+	def generate(self, parallelism):
+		id = 1
+		
+		id = self.gen_TmpltBank(id, parallelism)
+		id = self.gen_Inspiral_phase1(id, parallelism)
+		id = self.gen_Thinca_phase1(id, parallelism)
+		id = self.gen_TrigBank(id, parallelism)
+		id = self.gen_Inspiral_phase2(id, parallelism)
+		id = self.gen_Thinca_phase2(id, parallelism)
+			
+		print('Total task generated:', id-1)
+		
+		self.connect_TmpltBank_Inspiral_phase1()
+		self.connect_Inspiral_phase1_Thinca_phase1()
+		self.connect_Thinca_phase1_TrigBank()
+		self.connect_TrigBank_Inspiral_phase2()
+		self.connect_Inspiral_phase2_Thinca_phase2()
+		
+		# workflow generating
+		self.workflow = {'taskworkflow':[]}
+		self.workflow['taskworkflow'] += self.TmpltBank
+		self.workflow['taskworkflow'] += self.Inspiral_phase1
+		self.workflow['taskworkflow'] += self.Thinca_phase1
+		self.workflow['taskworkflow'] += self.TrigBank
+		self.workflow['taskworkflow'] += self.Inspiral_phase2
+		self.workflow['taskworkflow'] += self.Thinca_phase2
+		
+		json.dumps(self.workflow, indent=4)
+		filename = 'workflow LIGO {} tasks size {} parallelism {}.json'.format(id, self.task_size, parallelism)
+		json.dump(self.workflow, open(filename, encoding='utf8', mode='w'), indent=4)		
+	pass
 
 class MontageWorkflow:
+	task_size = 5
 	def gen_mProjectPP(self, id, parallelism):
 		tasks = []
 		for i in range(parallelism):
@@ -30,7 +386,7 @@ class MontageWorkflow:
 			task['taskname'] = tasknameGen('mProjectPP', id)
 			task['program'] = 'DummyTask.jar'
 			task['task_id'] = id
-			task['command'] = 60
+			task['command'] = self.task_size
 			task['_outE'] = []
 			task['_inE'] = []
 			
@@ -43,13 +399,14 @@ class MontageWorkflow:
 	
 	def gen_mDiffFit(self, id, parallelism):
 		totalTask = math.ceil((parallelism*(parallelism+1)/20))
+		totalTask -= 1
 		tasks = []
 		for i in range(totalTask):
 			task = {}
 			task['taskname'] = tasknameGen('mDiffFit', id)
 			task['program'] = 'DummyTask.jar'
 			task['task_id'] = id
-			task['command'] = 60
+			task['command'] = self.task_size
 			task['_outE'] = []
 			task['_inE'] = []
 			
@@ -65,7 +422,7 @@ class MontageWorkflow:
 		task['taskname'] = tasknameGen('mConcatFit', id)
 		task['program'] = 'DummyTask.jar'
 		task['task_id'] = id
-		task['command'] = 60
+		task['command'] = self.task_size
 		task['_outE'] = []
 		task['_inE'] = []
 		
@@ -78,7 +435,7 @@ class MontageWorkflow:
 		task['taskname'] = tasknameGen('mBgModel', id)
 		task['program'] = 'DummyTask.jar'
 		task['task_id'] = id
-		task['command'] = 60
+		task['command'] = self.task_size
 		task['_outE'] = []
 		task['_inE'] = []
 		
@@ -93,7 +450,7 @@ class MontageWorkflow:
 			task['taskname'] = tasknameGen('mBackground', id)
 			task['program'] = 'DummyTask.jar'
 			task['task_id'] = id
-			task['command'] = 60
+			task['command'] = self.task_size
 			task['_outE'] = []
 			task['_inE'] = []
 			
@@ -109,7 +466,7 @@ class MontageWorkflow:
 		task['taskname'] = tasknameGen('mImgTbl', id)
 		task['program'] = 'DummyTask.jar'
 		task['task_id'] = id
-		task['command'] = 60
+		task['command'] = self.task_size
 		task['_outE'] = []
 		task['_inE'] = []
 		
@@ -122,7 +479,7 @@ class MontageWorkflow:
 		task['taskname'] = tasknameGen('mAdd', id)
 		task['program'] = 'DummyTask.jar'
 		task['task_id'] = id
-		task['command'] = 60
+		task['command'] = self.task_size
 		task['_outE'] = []
 		task['_inE'] = []
 		
@@ -135,7 +492,7 @@ class MontageWorkflow:
 		task['taskname'] = tasknameGen('mShrink', id)
 		task['program'] = 'DummyTask.jar'
 		task['task_id'] = id
-		task['command'] = 60
+		task['command'] = self.task_size
 		task['_outE'] = []
 		task['_inE'] = []
 		
@@ -148,7 +505,7 @@ class MontageWorkflow:
 		task['taskname'] = tasknameGen('mJPEG', id)
 		task['program'] = 'DummyTask.jar'
 		task['task_id'] = id
-		task['command'] = 60
+		task['command'] = self.task_size
 		task['_outE'] = []
 		task['_inE'] = []
 		
@@ -264,8 +621,8 @@ class MontageWorkflow:
 		id = self.gen_mAdd(id)
 		id = self.gen_mShrink(id)
 		id = self.gen_mJPEG(id)
-		if id >= 100:
-			print('total task larger than 100')
+		
+		print('Total task generated:', id-1)
 		
 		# task node connecting
 		self.connect_mProjectPP_mDiffFit()
@@ -291,17 +648,19 @@ class MontageWorkflow:
 		self.workflow['taskworkflow'] += self.mJPEG
 		
 		json.dumps(self.workflow, indent=4)
-		json.dump(self.workflow, open('workflowTest.json', encoding='utf8', mode='w'), indent=4)
+		filename = 'workflow Montage {} tasks size {} parallelism {}.json'.format(id-1, self.task_size, parallelism)
+		json.dump(self.workflow, open(filename, encoding='utf8', mode='w'), indent=4)		
 		
 
 class EpigenmoicsWorkflow:
 	
+	task_size = 5
 	def gen_fastQSplit(self, id):
 		task = {}
 		task['taskname'] = tasknameGen('fastQSplit', id)
 		task['program'] = 'DummyTask.jar'
 		task['task_id'] = id
-		task['command'] = 60
+		task['command'] = self.task_size
 		task['_outE'] = []
 		task['_inE'] = []
 		
@@ -323,7 +682,7 @@ class EpigenmoicsWorkflow:
 			task['taskname'] = tasknameGen('filterContams', id)
 			task['program'] = 'DummyTask.jar'
 			task['task_id'] = id
-			task['command'] = 60
+			task['command'] = self.task_size
 			task['_outE'] = []
 			task['_inE'] = []
 			
@@ -348,7 +707,7 @@ class EpigenmoicsWorkflow:
 			task['taskname'] = tasknameGen('sol2sanger', id)
 			task['program'] = 'DummyTask.jar'
 			task['task_id'] = id
-			task['command'] = 60
+			task['command'] = self.task_size
 			task['_outE'] = []
 			task['_inE'] = []
 			
@@ -373,7 +732,7 @@ class EpigenmoicsWorkflow:
 			task['taskname'] = tasknameGen('fastq2bfq', id)
 			task['program'] = 'DummyTask.jar'
 			task['task_id'] = id
-			task['command'] = 60
+			task['command'] = self.task_size
 			task['_outE'] = []
 			task['_inE'] = []
 			
@@ -398,7 +757,7 @@ class EpigenmoicsWorkflow:
 			task['taskname'] = tasknameGen('map', id)
 			task['program'] = 'DummyTask.jar'
 			task['task_id'] = id
-			task['command'] = 60
+			task['command'] = self.task_size
 			task['_outE'] = []
 			task['_inE'] = []
 			
@@ -420,7 +779,7 @@ class EpigenmoicsWorkflow:
 		task['taskname'] = tasknameGen('mapMerge', id)
 		task['program'] = 'DummyTask.jar'
 		task['task_id'] = id
-		task['command'] = 60
+		task['command'] = self.task_size
 		task['_outE'] = []
 		task['_inE'] = []
 		
@@ -439,7 +798,7 @@ class EpigenmoicsWorkflow:
 		task['taskname'] = tasknameGen('mapIndex', id)
 		task['program'] = 'DummyTask.jar'
 		task['task_id'] = id
-		task['command'] = 60
+		task['command'] = self.task_size
 		task['_outE'] = []
 		task['_inE'] = []
 		
@@ -458,7 +817,7 @@ class EpigenmoicsWorkflow:
 		task['taskname'] = tasknameGen('pileup', id)
 		task['program'] = 'DummyTask.jar'
 		task['task_id'] = id
-		task['command'] = 60
+		task['command'] = self.task_size
 		task['_outE'] = []
 		task['_inE'] = []
 		
@@ -478,8 +837,8 @@ class EpigenmoicsWorkflow:
 		id = self.gen_mapMerge(id)
 		id = self.gen_mapIndex(id)
 		id = self.gen_pileup(id)
-		if id >= 100:
-			print('total task larger than 100')
+		
+		print('Total task generated:', id-1)
 		
 		# task node connecting
 		self.connect_fastQSplit_filterContams()
@@ -500,6 +859,10 @@ class EpigenmoicsWorkflow:
 		self.workflow['taskworkflow'] += self.mapMerge
 		self.workflow['taskworkflow'] += self.mapIndex
 		self.workflow['taskworkflow'] += self.pileup
+		
+		json.dumps(self.workflow, indent=4)
+		filename = 'workflow Epigenmoics {} tasks size {} parallelism {}.json'.format(id-1, self.task_size, num_parallel)
+		json.dump(self.workflow, open(filename, encoding='utf8', mode='w'), indent=4)		
 		
 	
 if __name__ == '__main__':
